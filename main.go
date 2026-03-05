fd6895d2024f6073c0b78194bdd1d97909d371bc
S. A. N. K. Bandara <mrsanbandaraprofessional@gmail.com>
2026-03-05 18:48:39 +0530

    feat: Implement port range scanning from 1 to 1024 and report all open ports.

--- main.go
+++ main.go
@@ -10,14 +10,21 @@ func main()  {
 	var host string
 		fmt.Printf("Enter the website or IP to scan: ")
 		fmt.Scanln(&host)
-	port := "80"
-	address := host + ":" + port
-	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
-	if err != nil {
-		fmt.Printf("Port %s is CLOSED on %s\n", port, host)
-		return
-	}
+	var openPorts[]int
 
-	defer conn.Close()
-	fmt.Printf("Port %s is OPEN on %s\n", port, host)
+	for i := 1; i <= 100; i++ {
+		port := fmt.Sprintf("%d", i)
+		address := host + ":" + port
+		conn, err := net.DialTimeout("tcp", address, 500*time.Millisecond)
+		if err != nil {
+			// fmt.Printf("Port %s is CLOSED on %s\n", port, host)
+			continue
+		}
+		conn.Close()
+		openPorts = append(openPorts, i)
+		// fmt.Printf("Port %s is OPEN on %s\n", port, host)
+	}
+	fmt.Printf("Scan complete! Open ports: %v\n", openPorts)
+
+
 }
