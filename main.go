e9ea121ca61a9f7ae01d2ee69e839c71cd1e4392
S. A. N. K. Bandara <mrsanbandaraprofessional@gmail.com>
2026-03-07 08:50:12 +0530

    fix: Correct results processing concurrency and reduce port scan timeout.

--- main.go
+++ main.go
@@ -16,22 +16,21 @@ func main()  {
 	var wg sync.WaitGroup
 
 	results := make(chan int)
+	
+	go func() {
+		for port := range results {
+			openPorts = append(openPorts, port)
+			fmt.Printf("Port %d is OPEN on %s\n", port, host)
+		}
+	}()
 
 	for i := 1; i <= 1024; i++ {
 		wg.Add(1)
-
-		go func() {
-			for port := range results {
-				openPorts = append(openPorts, port)
-				fmt.Printf("Port %d is OPEN on %s\n", port, host)
-			}
-		}()
-
 		go func(j int) {
 			defer wg.Done()
 			port := fmt.Sprintf("%d", j)
 			address := host + ":" + port
-			conn, err := net.DialTimeout("tcp", address, 2000*time.Millisecond)
+			conn, err := net.DialTimeout("tcp", address, 1000*time.Millisecond)
 			if err != nil {
 				return
 			}
